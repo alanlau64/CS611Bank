8381bfcc8213568ff5772e75cc0d46c4145f5ddd
Saravanan, Duruvan <dsaravanan3@gatech.edu>
2021-12-05 17:39:17 -0500

    cleanup

--- src/Customer.java
+++
@@ -1,6 +0,0 @@
-public class Customer extends User {
-
-    public Customer(String name, String password) {
-        super(name, password);
-    }
-}
--- src/Manager.java
+++
@@ -1,6 +0,0 @@
-public class Manager extends User {
-
-    public Manager(String name, String password) {
-        super(name, password);
-    }
-}
