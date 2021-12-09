d894b9385799e1184fd31772bd8d6abb286d56cb
Lightbringer <lighter0610@gmail.com>
2021-12-08 23:41:47 -0500

    implemented customerlog (haven't tested)

--- src/CustomerLog.java
+++ src/CustomerLog.java
@@ -1,7 +1,8 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -16,14 +17,43 @@ public class CustomerLog implements Log {
 
     @Override
     public void createLog (ArrayList managers) {
-        Gson gson = new Gson();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
         Type type = new TypeToken<ArrayList<Manager>>(){}.getType();
         this.content = gson.toJson(managers, type);
+        try {
+            this.logFile.createNewFile();
+            FileWriter writer = new FileWriter(logFile);
+            writer.write(this.content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public ArrayList<Manager> readLog() {
-        ArrayList<Manager> managers = new ArrayList<>();
-        return managers;
+    public ArrayList<Customer> readLog() {
+        ArrayList<Customer> customers = new ArrayList<>();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        try {
+            this.logFile.createNewFile();
+            BufferedReader reader = new BufferedReader(new FileReader(this.logFile));
+            StringBuilder str = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                str.append(line);
+                str.append(ls);
+            }
+            String json = str.toString();
+            Type type = new TypeToken<ArrayList<Manager>>(){}.getType();
+            customers = gson.fromJson(json, type);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return customers;
     }
 }
--- src/ManagerLog.java
+++ src/ManagerLog.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -17,6 +18,12 @@ public class ManagerLog implements Log {
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<User>>(){}.getType();
         this.content = gson.toJson(managers, type);
+        try {
+            logFile.createNewFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
